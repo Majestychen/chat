cb5d152304480d33461471f989ca1928326a65d4
zhao_hongsheng <zhao_hongsheng@microad-tech.com>
2017-03-06 16:47:46 +0800

    add socket.io front-end

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
+var path = require("path");
+
 var Koa = require("koa");
 var app = new Koa();
 
+// 静态文件支持
 var koaStatic = require("koa-static");
-var path = require("path");
-
-
 app.use(koaStatic(path.resolve(__dirname, ".")));
 
+// socket支持
+var server = require('http').createServer(app.callback());
+var io = require('socket.io')(server);
 
-app.listen(3000, function() {
+io.on("connection", function(socket){
+
+	console.log("get one connection ...");
+	
+	socket.on("from_client", function(clinetData){
+		console.log("get message from client:", clinetData);
+		socket.emit("news", "hello client !");
+	})
+
+	socket.on("disconnect", function(){
+		console.log("disconnect one connection ...");
+	})
+})
+
+server.listen(3000, function() {
 	console.log("chat server start on port [3000] ...");
 });
