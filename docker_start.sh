024223c3482715509b3a656b8e2bbdbadc71d1f7
zhao_hongsheng <yisurenali@aliyun.com>
2017-03-29 16:56:12 +0800

    update docker startup script

--- docker_start.sh
+++ docker_start.sh
@@ -10,7 +10,7 @@ echo "[chat] delete chat container"
 docker rm chat
 
 echo "[chat] run docker ..."
-docker run --rm -it -d -v /root/chat:/root/.pm2/logs/ -p 3005:3005 --name chat --link db:db chat
+docker run --rm -it -d -v /root/chat/log/:/root/.pm2/logs/ -p 3005:3005 --name chat --link db:db chat
 
 echo "[chat] start node server ..."
 docker exec -it chat /bin/bash -c "NODE_ENV=product pm2 start /root/chat/server/server.js" 
