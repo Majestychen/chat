65135039ee6b1719f2a51bb12c6b26bbe87f7478
zhao_hongsheng <zhao_hongsheng@microad-tech.com>
2017-04-14 16:31:56 +0800

    add ios style

--- js/index.js
+++ js/index.js
@@ -10,6 +10,8 @@ $(function() {
 	initSendBtn();
 	initWindowResize();
 
+	initOsSystem();
+
 	function initSocket() {
 
 		var host = window.location.hostname;
@@ -339,5 +341,32 @@ $(function() {
 		if (ele) {
 			ele.scrollIntoView();
 		}
+	}
+
+	function initOsSystem(){
+		var os = _getMobileOperatingSystem();
+		if(os == "IOS") {
+			$(document.body).addClass("ios");
+		}
+	}
+
+	function _getMobileOperatingSystem() {
+	  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+	      // Windows Phone must come first because its UA also contains "Android"
+	    if (/windows phone/i.test(userAgent)) {
+	        return "Windows Phone";
+	    }
+
+	    if (/android/i.test(userAgent)) {
+	        return "Android";
+	    }
+
+	    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+	    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+	        return "IOS";
+	    }
+
+	    return "unknown";
 	}
 })
